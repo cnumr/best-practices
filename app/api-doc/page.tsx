478908171fb7d575e9f540f50acd7172f5bfc090
Renaud Heluin <renaud.heluin@novagaia.fr>
2025-07-05 19:41:45 +0200

    fix: revert

--- app/api-doc/page.tsx
+++
@@ -1,7 +0,0 @@
-import ReactSwagger from './react-swagger';
-import { getApiDocs } from '../../lib/swagger';
-
-export default async function ApiDoc() {
-  const spec = await getApiDocs();
-  return <ReactSwagger spec={spec} />;
-}
--- app/api/versions/fiches.preval.ts
+++
@@ -1,8 +0,0 @@
-import { client } from '../../../tina/__generated__/databaseClient';
-import preval from 'next-plugin-preval';
-
-async function getData() {
-  return await client.queries.fichesConnection({ first: 1000 });
-}
-
-export default preval(getData());
