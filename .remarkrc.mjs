11b1fd3dd02552229a505f10817169b429e04065
Renaud Heluin <renaud.heluin@novagaia.fr>
2025-10-10 13:01:48 +0200

    feat: ajout de la configuration Remark pour la validation des fichiers Markdown et ajout de nouveaux schémas YAML pour la gestion des contenus

---
+++ .remarkrc.mjs
@@ -0,0 +1,27 @@
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkLintFrontmatterSchema from 'remark-lint-frontmatter-schema';
+
+const remarkConfig = {
+  plugins: [
+    remarkFrontmatter,
+    [
+      remarkLintFrontmatterSchema,
+      {
+        schemas: {
+          './content/fiche.schema.yaml': ['./src/content/fiches/**/*.{md,mdx}'],
+          './content/lexique.schema.yaml': [
+            './src/content/lexique/**/*.{md,mdx}',
+          ],
+          './content/personas.schema.yaml': [
+            './src/content/personas/**/*.{md,mdx}',
+          ],
+          './content/home.schema.yaml': ['./src/content/home/**/*.{md,mdx}'],
+          './content/default.schema.yaml': [
+            './src/content/mentionsLegales/**/*.{md,mdx}',
+          ],
+        },
+      },
+    ],
+  ],
+};
+export default remarkConfig;
